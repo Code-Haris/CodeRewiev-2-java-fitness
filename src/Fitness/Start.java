76e040567cb4b5c54dd177415bce323311013c97
Haris <learn@learntpoint.com>
2019-11-29 13:49:52 +0100

    Corrected

--- src/Fitness/Start.java
+++ src/Fitness/Start.java
@@ -1,5 +1,5 @@
 package Fitness;
-public class Main {
+public class Start {
 
     private static Exercise[] exercises = {
             new Exercise("Push-ups", "Place your hands on the floor. Raise up onto your toes so that all of your body weight is on your hands and your feet. Bend your elbows and lower your chest down toward the floor. Then, push off the floor and extend them so that you return to starting position.", 30, "Floor"),

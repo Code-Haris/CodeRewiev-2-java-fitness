3644901e2cf0a586f270974c73424775a83aecef
Haris <learn@learntpoint.com>
2019-11-29 13:48:18 +0100

    Finished CodeReview 2

---
+++ src/Fitness/Exercise.java
@@ -0,0 +1,38 @@
+package Fitness;
+
+
+public class Exercise {
+    private String name = "";
+    private String description = "";
+    private double duration = 0;
+    private String position = "";
+
+    public Exercise(String name, String description, double duration, String position) {
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    // BASE POINTS(3)
+    @Override
+    public String toString() {
+        return "\n" + this.getName() + "               " + "DURATION: " + this.getDuration()+".\n##################\nDESCRIPTION: " + this.getDescription();
+    }
+}
